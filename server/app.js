83b2d61bd7b5dc5d4afb991f99c50f81cfe3230e
Ben Deltenre <benjdelt@gmail.com>
2019-08-21 16:49:27 -0700

    Added file uploader

--- server/app.js
+++ server/app.js
@@ -1,7 +1,71 @@
-const express = require('express')
-const app = express()
-const port = 3001
+// const express = require('express')
+// const app = express()
+// const port = 3001
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+const express = require('express');
+const path = require('path');
+// const favicon = require('serve-favicon');
+// const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+
+const app = express();
+
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(logger('dev'));
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(fileUpload());
+app.use('/public', express.static(__dirname + '/public'));
+
+
+app.post('/upload', (req, res, next) => {
+  // console.log(req);
+  let imageFile = req.files.file;
+  console.log("imageFile: ", imageFile)
+  imageFile.mv(`${__dirname}/public/${req.body.filename}.jpg`, function(err) {
+    if (err) {
+      console.log("Error: ", err);
+      return res.status(500).send(err);
+    }
+    res.json({file: `public/${req.body.filename}.jpg`});
+  });
+
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send('error');
+});
+
+app.listen(3001, () => {
+  console.log('3001');
+});
+
+module.exports = app;
