902bae155c05f7ad745a5afc5a862ba6d3c4cddb
Ben Deltenre <benjdelt@gmail.com>
2019-08-21 17:08:44 -0700

    Removed filename input field, added default filename and cleaned up unused dependencies

--- server/app.js
+++ server/app.js
@@ -1,47 +1,28 @@
-// const express = require('express')
-// const app = express()
-// const port = 3001
-
-// app.get('/', (req, res) => res.send('Hello World!'))
-
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
 const express = require('express');
 const path = require('path');
-// const favicon = require('serve-favicon');
-// const logger = require('morgan');
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const cors = require('cors');
 
 const app = express();
 
-// view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-// app.use(logger('dev'));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
 app.use(fileUpload());
 app.use('/public', express.static(__dirname + '/public'));
 
 
 app.post('/upload', (req, res, next) => {
-  // console.log(req);
   let imageFile = req.files.file;
-  console.log("imageFile: ", imageFile)
-  imageFile.mv(`${__dirname}/public/${req.body.filename}.jpg`, function(err) {
+  const filename = 'image.jpg'
+  imageFile.mv(`${__dirname}/public/${filename}`, function(err) {
     if (err) {
-      console.log("Error: ", err);
       return res.status(500).send(err);
     }
-    res.json({file: `public/${req.body.filename}.jpg`});
+    res.json({file: `public/${filename}`});
   });
 
 })
@@ -61,7 +42,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.send('error');
+  res.send('Error 500');
 });
 
 app.listen(3001, () => {
