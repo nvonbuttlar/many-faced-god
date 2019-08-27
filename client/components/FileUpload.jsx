86b2321c652fca87d3f644b195a5e347affd1458
nvonbuttlar <nick.von.buttlar@gmail.com>
2019-08-27 15:23:06 -0700

    image uploader

---
+++ client/components/FileUpload.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+class FileUpload extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      imageURL: '',
+    };
+
+    this.handleUploadImage = this.handleUploadImage.bind(this);
+  }
+
+  handleUploadImage(ev) {
+    ev.preventDefault();
+
+    const data = new FormData();
+    data.append('file', this.uploadInput.files[0]);
+    fetch('http://localhost:3001/upload', {
+      method: 'POST',
+      body: data,
+    }).then((response) => {
+      response.json().then((body) => {
+        this.setState({ imageURL: `http://localhost:3001/${body.file}` });
+      });
+    });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleUploadImage}>
+        <div>
+          <input ref={(ref) => { this.uploadInput = ref; }} type="file" />
+        </div>
+        <br />
+        <div>
+          <button>Upload</button>
+        </div>
+        <img src={this.state.imageURL} alt="img" />
+      </form>
+    );
+  }
+}
+
+export default FileUpload;
--- server/app.js
+++ server/app.js
@@ -1,7 +1,53 @@
-const express = require('express')
-const app = express()
-const port = 3001
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const express = require('express');
+const path = require('path');
+// const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
+app.use(fileUpload());
+app.use('/public', express.static(__dirname + '/public'));
+
+
+app.post('/upload', (req, res, next) => {
+  let imageFile = req.files.file;
+  const filename = 'image.jpg'
+  imageFile.mv(`${__dirname}/public/${filename}`, function(err) {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.json({file: `public/${filename}`});
+  });
+
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send('Error 500');
+});
+
+app.listen(3001, () => {
+  console.log('3001');
+});
+
+module.exports = app;
